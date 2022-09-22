12bae587cf9be6789ab0c67b4290f25f8cdc59ba
yejin <kwonyj1022@naver.com>
2022-09-22 17:55:30 +0900

    JavaScript 셀프체크 2

---
+++ HTML+CSS+JS/3_JavaScript/3-Self_Check_2.js
@@ -0,0 +1,7 @@
+/* Self Check 1 */
+// 구구단 1단부터 9단까지 한 번에 출력하는 코드
+for (let dan = 1; dan <= 9; dan++){
+    for (let n = 1; n <= 9; n++){
+        console.log(`${dan} * ${n} = ${dan * n}`);
+    }
+}
