a5a2c897881cc136fb5c939be159ae532361f3c8
yejin <kwonyj1022@naver.com>
2022-10-04 16:39:49 +0900

    JavaScript 객체 속성 다루기

--- HTML+CSS+JS/3_JavaScript/3-11_Object_Properties.js
+++ HTML+CSS+JS/3_JavaScript/3-11_Object_Properties.js
@@ -20,33 +20,33 @@
         },
         "phone number":"010-000-0000"
     };
-    console.log(person["fullname"]);            // 출력결과 : Hong Gildong
-    console.log(person["age"]);                 // 출력결과 : 20
-    console.log(person["name"]);                // 출력결과 : { firstName: 'Gildong', lastName: 'Hong }
-    console.log(person["name"]["firstName"]);   // 출력결과 : Gildong
-    console.log(person["likes"]);               // 출력결과 : [ 'apple', 'samsung' ]
-    console.log(person["likes"][0]);            // 출력결과 : apple
-    console.log(person["likes"][1]);            // 출력결과 : samsung
-    console.log(person["printHello"]);          // 출력결과 : [Function: printHello]
-    console.log(person["printHello"]());        // 출력결과 : hello
-    console.log(person["phone number"]);        // 출력결과 : 010-000-0000
+    console.log(person["fullname"]);            // 실행결과 : Hong Gildong
+    console.log(person["age"]);                 // 실행결과 : 20
+    console.log(person["name"]);                // 실행결과 : { firstName: 'Gildong', lastName: 'Hong }
+    console.log(person["name"]["firstName"]);   // 실행결과 : Gildong
+    console.log(person["likes"]);               // 실행결과 : [ 'apple', 'samsung' ]
+    console.log(person["likes"][0]);            // 실행결과 : apple
+    console.log(person["likes"][1]);            // 실행결과 : samsung
+    console.log(person["printHello"]);          // 실행결과 : [Function: printHello]
+    console.log(person["printHello"]());        // 실행결과 : hello
+    console.log(person["phone number"]);        // 실행결과 : 010-000-0000
 
     /*
     마침표 연산자로 접근하기
     - 따옴표로 감싸지 않고 키를 바로 적음
     - 키 식별자에 공백이 있다면 마침표 연산자는 사용 불가 (대괄호 연산자로만 접근 가능)
     */
-    console.log(person.name.lastName);  // 출력결과 : Hong
-    console.log(person.age);            // 출력결과 : 20
-    console.log(person.likes[0]);       // 출력결과 : apple
-    console.log(person.printHello());   // 출력결과 : hello
+    console.log(person.name.lastName);  // 실행결과 : Hong
+    console.log(person.age);            // 실행결과 : 20
+    console.log(person.likes[0]);       // 실행결과 : apple
+    console.log(person.printHello());   // 실행결과 : hello
 
 /* 11-2 객체 속성 값 변경하기 */
     /*
     객체로 정의된 값을 바꾸고 싶다면 키로 속성에 접근해서 값을 재할당하면 됨
     */
     person.name = "Kim";
-    console.log(person.name);   // 출력결과 : Kim
+    console.log(person.name);   // 실행결과 : Kim
 
 /* 11-3 객체 속성 동적으로 추가하기 */
     /*
@@ -54,9 +54,9 @@
     객체 속성에 키로 접근해 값을 재할당할 때 해당 키가 객체에 없다면 해당 키와 값으로 구성된 새로운 속성이 객체에 추가됨
     */
     const obj = {};         // 빈 객체 생성
-    console.log(obj);       // 출력결과 : {}
+    console.log(obj);       // 실행결과 : {}
     obj.job = "teacher";    // job 속성 추가
-    console.log(obj);       // 출력결과 : { job: 'teacher' }
+    console.log(obj);       // 실행결과 : { job: 'teacher' }
 
 /* 11-4 객체 속성 동적으로 삭제하기 */
     /*
@@ -66,7 +66,7 @@
         name:"Hong Gildong"
     };
     delete obj2.name;
-    console.log(obj2);  // 출력결과 : {}
+    console.log(obj2);  // 실행결과 : {}
 
 /* 11-5 객체의 데이터 관리 방법 이해하기 */
     /*
@@ -86,8 +86,8 @@
     let num = 10;
     let copyNum = num;      // 변수 num의 데이터를 변수 conpyNum에 할당
     num = 20;               // 변수 num을 재할당
-    console.log(num);       // 출력결과 : 20
-    console.log(copyNum);   // 출력결과 : 10
+    console.log(num);       // 실행결과 : 20
+    console.log(copyNum);   // 실행결과 : 10
     
     // 얕은 복사 예시
     const man = {
@@ -95,7 +95,7 @@
     };
     const copyMan = man;        // 변수 man에 할당된 객체를 변수 conpyMan에 복사
     man.name = "Hong";          // 변수 man에 할당된 객체의 값을 변경
-    console.log(man.name);      // 출력결과 : Hong
-    console.log(copyMan.name);  // 출력결과 : Hong
+    console.log(man.name);      // 실행결과 : Hong
+    console.log(copyMan.name);  // 실행결과 : Hong
     // man과 copyMan은 같은 주소값을 가지기 때문에 man의 데이터를 변경하면 같은 주소를 바라보는 변수 copyMan의 데이터도 변경됨
     
