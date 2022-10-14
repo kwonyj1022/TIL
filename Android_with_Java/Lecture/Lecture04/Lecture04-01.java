c6180eaa2594a0bd2222593207a9ad950397f9da
yejin <kwonyj1022@naver.com>
2022-10-14 22:06:22 +0900

    최상위 뷰 - android:id

---
+++ Android_with_Java/Lecture/Lecture04/Lecture04-01.java
@@ -0,0 +1,18 @@
+// MainActivity.java 파일
+package com.example.lecture;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.id_test_view);
+        textView.setText("Hello World!");
+    }
+}
