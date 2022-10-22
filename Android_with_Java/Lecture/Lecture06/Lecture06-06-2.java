513a605afb569007d2fb3a795a54d0bd4b8f3b72
yejin <kwonyj1022@naver.com>
2022-10-22 10:30:44 +0900

    터치 이벤트 확장 - 클릭 리스너 - 좀 더 용이한 방법

---
+++ Android_with_Java/Lecture/Lecture06/Lecture06-06-2.java
@@ -0,0 +1,23 @@
+package com.example.lecture;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onMyButtonClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_view:
+                Toast.makeText(this, "OnClick Event!", Toast.LENGTH_LONG).show();
+                break
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
