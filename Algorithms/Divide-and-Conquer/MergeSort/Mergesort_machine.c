48d7ac1cb9739353fad3e5d12a7cbcfa13584e4e
yejin <kwonyj1022@naver.com>
2022-11-24 19:19:30 +0900

    [Divide & Conquer] Merge Sort (machine)

--- Algorithms/Divide-and-Conquer/MergeSort/Mergesort_machine.c
+++ Algorithms/Divide-and-Conquer/MergeSort/Mergesort_machine.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #define N 10    // 정렬할 배열의 크기 (요소 개수)
-void merge(int h, int m, const int U[], const int V[], int S[]);
+int MIN;
 void mergesort(int n, int S[]);
-void printResult(int n, int S[]);
+void merge(int h, int m, const int U[], const int V[], int S[]);
+void printUV(int n, const int S[], const int U[], const int V[]);
+void printUVMerge(int n, const int U[], const int V[], const int S[]);
+void printResult(int n, const int S[]);
 
 int main(void) {
     int S[N] = { 5, 2, 0, 7, 1, 8, 10, 15, 12, 4 };
+
+    for (int i = 0; i < N; i++) {
+        if (S[i] < MIN)
+            MIN = S[i];
+    }
+
     mergesort(N, S);
     printResult(N, S);
     return 0;
@@ -16,16 +25,14 @@ void mergesort(int n, int S[]) {
         const int h = n / 2;
         const int m = n - h;
 
-        int U[N] = { NULL };
-        int V[N] = { NULL };
+        int U[N];
+        int V[N];
 
-        printf("\n배열 S : ");
         for (int i = 0; i < N; i++) {
-            if (S[i] == NULL)
-                break;
-            printf("%d ", S[i]);
+            U[i] = MIN - 1;
+            V[i] = MIN - 1;
         }
-        printf("에 대한 U, V");
+
         for (int i = 0; i < h; i++) {
             U[i] = S[i];
         }
@@ -33,19 +40,7 @@ void mergesort(int n, int S[]) {
             V[i] = S[h + i];
         }
 
-        printf("\nU배열 : ");
-        for (int i = 0; i < h; i++) {
-            if (U[i] == NULL)
-                break;
-            printf("%d ", U[i]);
-        }
-        printf("\nV배열 : ");
-        for (int i = 0; i < m; i++) {
-            if (V[i] == NULL)
-                break;
-            printf("%d ", V[i]);
-        }
-        printf("\n");
+        printUV(N, S, U, V);
 
         mergesort(h, U);
         mergesort(m, V);
@@ -79,28 +74,59 @@ void merge(int h, int m, const int U[], const int V[], int S[]) {
         }
     }
 
+    printUVMerge(N, U, V, S);
+}
+
+void printUV(int n, const int S[], const int U[], const int V[]) {
+    const int h = n / 2;
+    const int m = n - h;
+
+    printf("\n배열 S : ");
+    for (int i = 0; i < N; i++) {
+        if (S[i] < MIN)
+            break;
+        printf("%d ", S[i]);
+    }
+    printf("에 대한 U, V");
+
+    printf("\nU배열 : ");
+    for (int i = 0; i < h; i++) {
+        if (U[i] < MIN)
+            break;
+        printf("%d ", U[i]);
+    }
+    printf("\nV배열 : ");
+    for (int i = 0; i < m; i++) {
+        if (V[i] < MIN)
+            break;
+        printf("%d ", V[i]);
+    }
+    printf("\n");
+}
+
+void printUVMerge(int n, const int U[], const int V[], const int S[]) {
     printf("\n배열 U = { ");
     for (int i = 0; i < N; i++) {
-        if (U[i] == NULL)
+        if (U[i] < MIN)
             break;
         printf("%d ", U[i]);
     }
     printf("}와 배열 V = { ");
     for (int i = 0; i < N; i++) {
-        if (V[i] == NULL)
+        if (V[i] < MIN)
             break;
         printf("%d ", V[i]);
     }
     printf("}를 merge 하면 \n{ ");
     for (int i = 0; i < N; i++) {
-        if (S[i] == NULL)
+        if (S[i] < MIN)
             break;
         printf("%d ", S[i]);
     }
     printf("}\n\n===============================================================\n");
 }
 
-void printResult(int n, int S[]) {
+void printResult(int n, const int S[]) {
     printf("\n\n\t[최종 정렬 결과]\n\t S = ");
     for (int i = 0; i < N; i++) {
         printf("%d ", S[i]);
