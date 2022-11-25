43caf67d0b879c490bda5723d96c738ba9e5fa69
yejin <kwonyj1022@naver.com>
2022-11-26 02:06:45 +0900

    다른 앱을 실행하는 예제

---
+++ Android_with_Java/Lecture/Lecture07/Lecture07-01-a.java
@@ -0,0 +1,33 @@
+// A앱 MainActivity.java 파일
+package com.example.a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        // 1. 인텐트를 하나 생성한다.
+        Intent intent = new Intent();
+
+        // 2. 인텐트에 실행할 패키지의 액티비티 정보를 설정한다.
+        ComponentName componentName = new ComponentName(
+                "com.example.b",
+                "com.example.b.MainActivity");
+
+        intent.setComponent(componentName);
+
+        // 3. B앱의 액티비티를 실행한다.
+        startActivity(intent);
+    }
+}
