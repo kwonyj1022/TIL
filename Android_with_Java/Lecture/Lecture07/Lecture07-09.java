1fc52631e94beaa437b36eb0747b2a787ec51ef8
yejin <kwonyj1022@naver.com>
2022-12-04 10:32:21 +0900

    인텐트 - 암시적 인텐트 (액션과 데이터 위치)

---
+++ Android_with_Java/Lecture/Lecture07/Lecture07-09.java
@@ -0,0 +1,31 @@
+package com.example.a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        // 1. 인텐트를 하나 생성한다.
+        Intent intent = new Intent();
+
+        // 2. 외부 이메일 컴포넌트를 요청한다.
+            // 어떤 데이터를 보여달라는 액션
+        intent.setAction(Intent.ACTION_VIEW);
+            // 다음 위치의 어떤 데이터를 보여달라는 의미
+        intent.setData(Uri.parse("http://m.naver.com"));
+
+        // 3. 외부 컴포넌트를 실행한다.
+        startActivity(intent);
+    }
+}
