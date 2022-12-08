420feb8908a45da574f2398295123ae1fc56d4c8
yejin <kwonyj1022@naver.com>
2022-12-08 22:01:24 +0900

    새로운 태스크 생성 - FLAG_ACTIVITY_MULTIPLE_TASK 인텐트 플래그

---
+++ Android_with_Java/Lecture/Lecture07/Lecture07-24-A1.java
@@ -0,0 +1,25 @@
+package com.example.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class A1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a1);
+    }
+
+    public void onClick (View v) {
+        Intent intent = new Intent(this, A2Activity.class);
+        // FLAG_ACTIVITY_MULTIPLE_TASK : FLAG_ACTIVITY_NEW_TASK의 보조 플래그 (단독 사용 불가)
+        // FLAG_ACTIVITY_NEW_TASK와 FALG_ACTIVITY_MULTIPLE_TASK를 함께 사용하여 액티비티를 실행하면 실행되는 액티비티는 무조건 새로운 태스크 생성
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+        startActivity(intent);
+    }
+}
