2d263cc1e6920eaa1dd3deac6ace4171e89cc04d
yejin <kwonyj1022@naver.com>
2022-12-08 23:46:16 +0900

    액티비티 실행모드 - singleTask

---
+++ Android_with_Java/Lecture/Lecture07/Lecture07-29-B1.java
@@ -0,0 +1,27 @@
+package com.example.taskb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class B1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick (View view) {
+        Intent intent = new Intent();
+        ComponentName componentName = new ComponentName(
+                "com.example.taskb",
+                "com.example.taskb.A2Activity");
+        intent.setComponent(componentName);
+
+        startActivity(intent);
+    }
+}
