f60fd2bcb51ff303d48498264fa502c01c0a62f2
yejin <kwonyj1022@naver.com>
2022-12-08 22:11:02 +0900

    액티비티 실행모드 - standard

---
+++ Android_with_Java/Lecture/Lecture07/Lecture07-25-A3.java
@@ -0,0 +1,20 @@
+package com.example.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class A3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a3);
+    }
+
+    public void onClick (View v) {
+        startActivity(new Intent(this, A3Activity.class));
+    }
+}
