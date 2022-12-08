07959d389d039f8d7684e921baafb05bebb7e8a5
yejin <kwonyj1022@naver.com>
2022-12-08 21:55:55 +0900

    새로운 태스크 생성 - FLAG_ACTIVITY_NEW_TASK 인텐트 플래그

--- Android_with_Java/Lecture/Lecture07/Lecture07-23-A2.java
+++ Android_with_Java/Lecture/Lecture07/Lecture07-23-A2.java
@@ -16,7 +16,7 @@ public class A2Activity extends AppCompatActivity {
 
     public void onClick (View v) {
         Intent intent = new Intent(this, A3Activity.class);
-        // FLAG_ACTIVITY-NEW_TASK : 자신의 태스크 친화력 이름 태스크에서만 실행됨.
+        // FLAG_ACTIVITY_NEW_TASK : 자신의 태스크 친화력 이름 태스크에서만 실행됨.
         // 기존에 액티비티의 태스크 친화력 이름을 가진 태스크가 존재하지않으면, 태스크 친화력 이름으로 새로운 태스크를 생성하고 그 안에서 액티비티가 실행됨
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
