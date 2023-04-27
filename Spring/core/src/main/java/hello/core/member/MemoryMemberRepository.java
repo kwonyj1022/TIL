e8af02242db1e3d403e85f5da51d7c8789db7ebe
kwonyj1022 <kwonyj1022@naver.com>
2023-04-27 16:34:19 +0900

    Spring Core Basic - 회원 도메인 개발

---
+++ Spring/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
