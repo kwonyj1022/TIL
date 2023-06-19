0458697afd6f11a2b8a5ab287994a6beafec932a
kwonyj1022 <kwonyj1022@naver.com>
2023-06-19 23:04:36 +0900

    Spring Core Basic - 새로운 할인 정책 적용과 문제점

--- Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,13 +1,13 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 
 public class OrderServiceImpl implements OrderService {
 
     private MemberRepository memberRepository = new MemoryMemberRepository();
-    private DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
