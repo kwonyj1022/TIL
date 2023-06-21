19d6c022c2f0f30065a645b59e6f58760dd00cee
kwonyj1022 <kwonyj1022@naver.com>
2023-06-22 05:53:47 +0900

    Spring Core Basic - Configuration과 싱글톤

---
+++ Spring/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,28 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        assertThat(memberRepository1).isSameAs(memberRepository);
+        assertThat(memberRepository2).isSameAs(memberRepository);
+    }
+}
