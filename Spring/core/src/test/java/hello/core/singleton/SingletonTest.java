aa25f2ee1759b744860eec3ebd6f4532440a55ea
kwonyj1022 <kwonyj1022@naver.com>
2023-06-22 04:57:34 +0900

    Spring Core Basic - 스프링 없는 순수한 DI 컨테이너의 문제점

---
+++ Spring/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,20 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
