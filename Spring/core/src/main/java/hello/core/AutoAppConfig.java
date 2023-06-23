cec804e3cde6768502c78eb993e64ea94392b72d
kwonyj1022 <kwonyj1022@naver.com>
2023-06-23 23:58:55 +0900

    Spring Core Basic - 필터

--- Spring/core/src/main/java/hello/core/AutoAppConfig.java
+++ Spring/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
         // 기존의 AppConfig, test 시 TestConfig 등 @Configuration 붙은 파일들이 등록되지 않도록 하기 위해 아래 설정 추가
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
